abf5d3df4cc8be9a9c09b61d028bfbea4f09d37c
CaiafaTR <michel.caiafa@gmail.com>
2023-08-25 17:56:15 -0300

    feat: add main program logic.

--- calculadora_imc/bin/calculadora_imc.dart
+++ calculadora_imc/bin/calculadora_imc.dart
@@ -1,6 +1,25 @@
 import 'package:calculadora_imc/classes/person.dart';
+import 'dart:convert';
+import 'dart:io';
 
 void main(List<String> arguments) {
-  var m = Person('Michel', -1, 1.75);
-  print(m.getResultIMC());
+  print("Enter your name:");
+  String name = stdin.readLineSync(encoding: utf8) ?? "";
+  print("Enter your weight:");
+  String weight = stdin.readLineSync(encoding: utf8) ?? "";
+  print("Enter your height:");
+  String height = stdin.readLineSync(encoding: utf8) ?? "";
+  if(name.isEmpty){
+    print('Name cannot be empty');
+  }
+  else if(weight.isEmpty){
+    print('Weight cannot be empty');
+  }
+  else if(height.isEmpty){
+    print('Height cannot be empty');
+  }
+  else{
+    var person = Person(name, double.parse(weight), double.parse(height));
+    print(person.getResultIMC());
+  }
 }
